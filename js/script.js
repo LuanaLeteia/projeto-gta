069a21f7efd6fbbef3bc51770000f8e23f18aef6
Luana Letéia <luanaleteia@gmail.com>
2026-08-20 19:57:34 -0300

    Adicionado interação video

--- js/script.js
+++ js/script.js
@@ -33,3 +33,40 @@ blocos.forEach(function(bloco){
 
 
 // INTERAÇÃO VIDEO COM O SCROLL
+
+gsap.registerPlugin(ScrollTrigger);
+
+const video = document.querySelector(".capa-video");
+
+function animarCapa(){
+    const linhaDoTempo = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".capa",
+            start: "top top",
+            end: "+=2500",
+            scrub: 1,
+            pin: true
+        }
+    });
+
+    linhaDoTempo.to(".capa-conteudo, .capa-barra, .capa-seta", {
+        opacity: 0,
+        scale: 0.6,
+        duration: 0.
+    }, 0);
+
+    linhaDoTempo.to(video, {opacity: 1, duration: 0.8}, 0);
+
+    linhaDoTempo.to(video, {
+        currentTime: video.duration,
+        duration: 1,
+        ease: "none"
+    }, 0);
+};
+
+if (video.readyState >= 1){
+    animarCapa();
+}else{
+    video.addEventListener("loadedmetadata", animarCapa);
+};
+
