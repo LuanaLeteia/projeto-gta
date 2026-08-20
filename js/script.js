ec0e9f8fe35896d2a79655294dadb42672fdc4fe
Luana Letéia <luanaleteia@gmail.com>
2026-08-19 21:43:11 -0300

    Adicionado JS - interação do menu

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// MENU
+
+const menu = document.getElementById("menu");
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 50){
+        menu.classList.add("menu-rolado");
+    } else{
+        menu.classList.remove("menu-rolado");
+    }
+
+    
+})
+
+// INTERAÇÃO DOS BLOCOS
+
+// INTERAÇÃO VIDEO COM O SCROLL
